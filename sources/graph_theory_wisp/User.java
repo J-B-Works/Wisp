36df1ff5c8804bb3ad45cee88c3072e864eec288
Julia Andrade <andradejulia416@gmail.com>
2026-04-01 11:02:33 -0300

    chore: add user refactor

--- sources/graph_theory_wisp/User.java
+++ sources/graph_theory_wisp/User.java
@@ -1,3 +1,24 @@
+/*
+
+=======================================================
+  WISP– Sistema de recomendação de educação e cultura
+=======================================================
+
+-- Grupo --
+Teoria dos Grafos - Turma: 6G
+- Bruna Gonçalves Corte David (RA: 10425696)
+- Júlia Andrade (RA: 1042513)
+
+-- Síntese do Conteúdo --
+
+-> Classe que representa um nó do tipo Usuário no grafo
+
+-- Histórico de Alterações --
+
+26/03/2026 - Bruna - Cria classe User.java para representar o nó do tipo "Usuário"
+
+*/
+
 package graph_theory_wisp;
 
 import java.util.*;
